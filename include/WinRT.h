8730e91a94cdaadc5468acec7ad3eb0c18e0c9cc
grambbledook <vodopyanovd@gmail.com>
2024-07-15 18:21:07 +0200

    not working setup

---
+++ include/WinRT.h
@@ -0,0 +1,5 @@
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Devices.Bluetooth.h>
+#include <winrt/Windows.Devices.Bluetooth.Advertisement.h>
+#include <winrt/Windows.Devices.Bluetooth.GenericAttributeProfile.h>
+#include <winrt/Windows.Devices.Enumeration.h>
