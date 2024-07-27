0a12d98c0375a37159165eb17be0630f957edc3f
grambbledook <vodopyanovd@gmail.com>
2024-07-27 18:00:08 +0200

    delete unused python code

--- hud/__init__.py
+++
@@ -1,5 +0,0 @@
-from PySide6.QtAsyncio import QAsyncioTask
-
-# Monkey patch QtAsyncio to allow Bleak connections to be established w/o 'QtTask.xxx is not implemented' error
-QAsyncioTask.cancelling = lambda self: False
-QAsyncioTask.uncancel = lambda self: 999
